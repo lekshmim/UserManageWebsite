910b0d72f41162bf07457b69a071939b1a368d14
Lekshmi M <mnair.lekshmi@gmail.com>
2012-03-01 20:20:17 +0300

    first commit

---
+++ UserManagement/Code_User Management/Controllers/UserRoleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserManagement.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to User Management System";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
